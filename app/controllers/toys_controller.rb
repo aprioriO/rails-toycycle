bc083ef2f5ecbacd74c2b8b9a58edecf774c9542
Mustafa Farah <mustafafarah8@hotmail.com>
2025-01-28 16:13:48 +0000

    Updated routes, generated toys & pages controller and associated view pages and added validations

---
+++ app/controllers/toys_controller.rb
@@ -0,0 +1,9 @@
+class ToysController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+end
--- app/models/toy.rb
+++ app/models/toy.rb
@@ -3,4 +3,7 @@ class Toy < ApplicationRecord
 
   has_many :trades_as_seeker, class_name: "Trade", foreign_key: :toy_seeker_id
   has_many :trades_as_trader, class_name: "Trade", foreign_key: :toy_trader_id
+
+  validates :name, :location, :category, :description, :condition, :status, :need_in_return, presence:true
+
 end
