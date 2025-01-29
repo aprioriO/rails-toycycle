17433a13a39f1c9d9298213e97fd31dbc2e379b4
Leea Berry <leeaberry@gmail.com>
2025-01-29 14:01:49 +0000

    added user controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
   get "howtouse", to: "pages#howtouse"
   get "dashboard", to: "pages#dashboard"
+  get "users/:id", to: "users#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
