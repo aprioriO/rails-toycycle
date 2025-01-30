931833921bff4a8f5b9ba3fcb27549286eebb191
Mustafa Farah <mustafafarah8@hotmail.com>
2025-01-30 16:54:01 +0000

    add ney toy updates

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -17,4 +17,19 @@ class ToysController < ApplicationController
     @toy = Toy.new
   end
 
+  def create
+    @toy = Toy.new(toy_params)
+    if @toy.save
+      redirect_to toys_path, notice: "Toy created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def toy_params
+    params.require(:toy).permit(:name, :location, :category, :description, :condition, :image)
+  end
+
 end
