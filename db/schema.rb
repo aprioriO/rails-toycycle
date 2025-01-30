49654ffcdc3ca5612bd597a4bc394c01c0a4afd9
Leea Berry <leeaberry@gmail.com>
2025-01-30 11:41:30 +0000

    merging master

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_29_115323) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_29_160034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.string "comment"
@@ -40,17 +68,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_29_115323) do
   end
 
   create_table "trades", force: :cascade do |t|
-    t.bigint "seeker_toy_id"
-    t.bigint "trader_toy_id"
-    t.bigint "trader_id"
-    t.bigint "seeker_id"
+    t.bigint "toy_seeker_id"
+    t.bigint "toy_trader_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status", default: "pending", null: false
-    t.index ["seeker_id"], name: "index_trades_on_seeker_id"
-    t.index ["seeker_toy_id"], name: "index_trades_on_seeker_toy_id"
-    t.index ["trader_id"], name: "index_trades_on_trader_id"
-    t.index ["trader_toy_id"], name: "index_trades_on_trader_toy_id"
+    t.index ["toy_seeker_id"], name: "index_trades_on_toy_seeker_id"
+    t.index ["toy_trader_id"], name: "index_trades_on_toy_trader_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -76,13 +100,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_29_115323) do
     t.index ["user_id"], name: "index_watchlists_on_user_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "reviews", "toys"
   add_foreign_key "reviews", "users"
   add_foreign_key "toys", "users"
-  add_foreign_key "trades", "toys", column: "seeker_toy_id"
-  add_foreign_key "trades", "toys", column: "trader_toy_id"
-  add_foreign_key "trades", "users", column: "seeker_id"
-  add_foreign_key "trades", "users", column: "trader_id"
+  add_foreign_key "trades", "toys", column: "toy_seeker_id"
+  add_foreign_key "trades", "toys", column: "toy_trader_id"
   add_foreign_key "watchlists", "toys"
   add_foreign_key "watchlists", "users"
 end
