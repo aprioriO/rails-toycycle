edbe15deb56d13debb777dd5af5ac85fdce6619c
Mustafa Farah <mustafafarah8@hotmail.com>
2025-02-04 10:43:39 +0000

    Added review to profile page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :toys
+  has_many :reviews
 
   has_many :trades_as_trader, class_name: "Trade", foreign_key: :trader_id
   has_many :trades_as_seeker, class_name: "Trade", foreign_key: :seeker_id
